4d758d157dbfaf0eb4b73f2370311d4a7471365b
Jakub Grzegorzewski <kgrzegorzewski@hypnoticants.com>
2025-07-26 17:08:54 +0200

    innit

---
+++ src/test/java/grzegorzewski/roadtosuccesbackend/RoadToSuccesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package grzegorzewski.roadtosuccesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadToSuccesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
